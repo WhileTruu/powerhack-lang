4228a71871ad7308eb3ac134ad6576422a144279
Marten Truu <marten.truu@gmail.com>
2022-08-06 10:14:21 +0300

    Add unused test file/folder TODO

--- cli/tests/Example.elm
+++ cli/tests/Example.elm
@@ -1,10 +1,9 @@
 module Example exposing (..)
 
-import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
 
 suite : Test
 suite =
+    -- TODO remove the file if it's not used?
     todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
